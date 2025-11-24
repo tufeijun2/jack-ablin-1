cabd9cc6e905ebe7359ce60c233230e9a8d2a1b1
17716138933 <503332133@qq.com>
2025-11-25 06:03:47 +0800

    998

--- src/index.js
+++
@@ -1,8 +0,0 @@
-// src/index.js（空文件即可，无需任何代码）
-export default {
-  async fetch(request, env, ctx) {
-    // 无需手动处理路由，not_found_handling 会自动转发 404 到 index.html
-    // 静态文件会自动从 public 目录加载
-    return env.ASSETS.fetch(request);
-  },
-};
